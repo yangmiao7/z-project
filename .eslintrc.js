7f49f17467a1d4d87da1924b79e5d6e04a4a2e36
Yami <ym.vip@foxmail.com>
2020-02-15 00:34:12 +0800

    :heavy_plus_sign: chore: 添加eslint-config-standard相关配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,15 +2,24 @@ module.exports = {
   parser:  '@typescript-eslint/parser', //定义ESLint的解析器
   extends: [
     'plugin:react/recommended' ,       //用于检测tsx文件
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    "standard",
+    "standard-jsx"
   ],//定义文件继承的子规范
-  plugins: ['@typescript-eslint'],//定义了该eslint文件所依赖的插件
+  plugins: ['@typescript-eslint', 'react-hooks'],//定义了该eslint文件所依赖的插件
   env:{                          //指定代码的运行环境
     browser: true,
     node: true,
   },
   rules: {
-    indent: ['error', 2]
+    indent: ['error', 2],
+    'jsx-quotes': [2, 'prefer-single'],
+    'quotes': [2, 'single', {
+      'avoidEscape': true,
+      'allowTemplateLiterals': true
+    }],
+    "react-hooks/rules-of-hooks": "error", // 检查 Hook 的规则
+    "react-hooks/exhaustive-deps": "warn" // 检查 effect 的依赖
   },
   settings: {             //自动发现React的版本，从而进行规范react代码
     "react": {
