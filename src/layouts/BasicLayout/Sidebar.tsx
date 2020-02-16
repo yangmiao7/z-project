d190d30c596195798b65f0b5eacab59a88be72c3
Yami <ym.vip@foxmail.com>
2020-02-16 18:06:27 +0800

    :sparkles: feat: 简易的侧边栏逻辑，后期改为配置化生成

--- src/layouts/BasicLayout/Sidebar.tsx
+++ src/layouts/BasicLayout/Sidebar.tsx
@@ -1,16 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Menu, Icon } from 'antd'
 import './Sidebar.less'
 import { ClickParam } from 'antd/es/menu'
+import { useHistory, useLocation } from 'react-router-dom'
+
 const { SubMenu } = Menu
 
 export interface SidebarProps {
-    collapsed: boolean;
+  collapsed: boolean;
 }
 
 export default function Sidebar (props: SidebarProps): React.ReactElement {
+  const history = useHistory()
+  const location = useLocation()
+  const [openKeys, setOpenKeys] = useState<string[]>([])
+
+  useEffect(() => {
+    const pathnameList = location.pathname.split('/')
+    openKeys.length <= 0 && setOpenKeys([`/${pathnameList[1]}`])
+  }, [location, openKeys.length])
+
   const handleClickMenuItem = (payload: ClickParam): void => {
-    console.log(payload)
+    history.push(payload.key)
+  }
+
+  const handleSubMenuOpen = (openKeys: string[]): void => {
+    setOpenKeys(openKeys)
   }
 
   return (
@@ -20,19 +35,21 @@ export default function Sidebar (props: SidebarProps): React.ReactElement {
       </div>
 
       <Menu
-        defaultSelectedKeys={['1']}
+        defaultSelectedKeys={[location.pathname]}
+        openKeys={openKeys}
         mode='inline'
         theme='dark'
         inlineCollapsed={props.collapsed}
         onClick={handleClickMenuItem}
+        onOpenChange={handleSubMenuOpen}
       >
-        <Menu.Item key='1'>
+        <Menu.Item key='/home'>
           <Icon type='home' />
           <span>首页</span>
         </Menu.Item>
 
         <SubMenu
-          key='sub1'
+          key='/system'
           title={
             <span>
               <Icon type='setting' />
@@ -40,9 +57,9 @@ export default function Sidebar (props: SidebarProps): React.ReactElement {
             </span>
           }
         >
-          <Menu.Item key='5'>组织架构</Menu.Item>
-          <Menu.Item key='6'>员工管理</Menu.Item>
-          <Menu.Item key='7'>功能权限</Menu.Item>
+          <Menu.Item key='/system/organizational'>组织架构</Menu.Item>
+          <Menu.Item key='/system/employee'>员工管理</Menu.Item>
+          <Menu.Item key='/system/functionality'>功能权限</Menu.Item>
         </SubMenu>
       </Menu>
     </aside>
